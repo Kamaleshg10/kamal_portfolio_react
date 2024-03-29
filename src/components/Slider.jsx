5b21b213cabc16b6fa8d8fb8938f50f68984abb9
kamalesh <mechanical14358@gmail.com>
2024-03-29 10:27:23 +0530

    video correction

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,24 +13,24 @@ export const Slider = () => {
             <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">                
                 {/* <video src="video/bg1.mp4" autoPlay loop /> */}
-                <video src={video} autoPlay loop />
-                {/* <img src="img/slide1.jpg" className="d-inline-block w-100 " alt="images"/> */}
+                {/* <video src={video} autoPlay loop /> */}
+                <img src="img/slide1.jpg" className="d-inline-block w-100 " alt="images"/>
                 {/* <div className="carousel-caption  d-none d-md-block">
                     <h5 className='text-white'>First slide label</h5>
                     <p className='text-white'>Some representative placeholder content for the first slide.</p>
                 </div> */}
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                <video src="video/bg4.mp4" autoPlay loop  />
-                {/* <img src="img/slide2.jpg" className="d-inline-block w-100 " alt="images"/> */}
+                {/* <video src="video/bg4.mp4" autoPlay loop  /> */}
+                <img src="img/slide2.jpg" className="d-inline-block w-100 " alt="images"/>
                 {/* <div className="carousel-caption d-none d-md-block">
                     <h5 className='text-white'>Second slide label</h5>
                     <p className='text-white'>Some representative placeholder content for the second slide.</p>
                 </div> */}
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                <video src="video/bg3.mp4" autoPlay loop />
-                {/* <img src="img/slide3.jpg" className="d-inline-block w-100" alt="images"/> */}
+                {/* <video src="video/bg3.mp4" autoPlay loop /> */}
+                <img src="img/slide3.jpg" className="d-inline-block w-100" alt="images"/>
                 {/* <div className="carousel-caption d-none d-md-block">
                     <h5 className='text-white'>Third slide label</h5>
                     <p className='text-white'>Some representative placeholder content for the third slide.</p>
